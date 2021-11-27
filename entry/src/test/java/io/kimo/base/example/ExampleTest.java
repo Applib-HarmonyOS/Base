e797182c63c73541179160ae9a77025929361e17
SuryAjit <suryajith1983@gmail.com>
2021-11-27 20:30:15 +0800

    updated comment in both test case

--- entry/src/test/java/io/kimo/base/example/ExampleTest.java
+++ entry/src/test/java/io/kimo/base/example/ExampleTest.java
@@ -2,8 +2,12 @@ package io.kimo.base.example;
 
 import org.junit.Test;
 
+/**
+ * ExampleTest
+ */
 public class ExampleTest {
     @Test
     public void onStart() {
+        // Do nothing to start the example test
     }
 }
